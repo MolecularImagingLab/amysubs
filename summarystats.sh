ae10d767d283e4cc772a0315eef8a33ba762ba94
ramroomh <rhamati95@gmail.com>
2020-11-26 13:32:36 -0500

    added extra summary stats for whole amygdala

--- summarystats.sh
+++ summarystats.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
-
+# load subjects list
 subjects=$( cat subjectlist.txt )
+# Group subnuclei results by hemisphere (produced by freesurfer segmentation script)
 sides='lh rh'
 for side in ${sides[@]}; do
 
 	asegstats2table --subjects $subjects --statsfile=amygdalar-nuclei.$side.T2.v21.T2.stats --tablefile=$side.amy_allsubjects.txt --skip
 
 done
+# Group whole amygdala result (produed by freesurfer reconall script)
+asegstats2table --subjects $subjects --statsfile=aseg.stats --tablefile=whole.amy_allsubjects.txt --skip
